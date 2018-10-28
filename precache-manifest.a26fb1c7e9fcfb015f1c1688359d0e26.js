b3a8b62baa53d19966412238836b57107f10bac9
Santiago Beloqui <beloquisantiago@gmail.com>
2018-10-28 20:53:00 -0300

    Updates

---
+++ precache-manifest.a26fb1c7e9fcfb015f1c1688359d0e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "78ee30faaf6fb225fdfe",
+    "url": "/drum-machine/static/js/main.78ee30fa.chunk.js"
+  },
+  {
+    "revision": "fd97677c7b9454c5e64f",
+    "url": "/drum-machine/static/js/1.fd97677c.chunk.js"
+  },
+  {
+    "revision": "78ee30faaf6fb225fdfe",
+    "url": "/drum-machine/static/css/main.7edb8941.chunk.css"
+  },
+  {
+    "revision": "1a8b61da9797c1eb748f30c56b5d70b8",
+    "url": "/drum-machine/index.html"
+  }
+];
